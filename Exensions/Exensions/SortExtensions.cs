72e9f9d59b81ab51bb8cac96b3edef150b151ee8
Lyle Hart III <lyle.hart.3@gmail.com>
2016-03-19 20:31:12 -0400

    initial commit

---
+++ Exensions/Exensions/SortExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exensions
+{
+    public static class SortExtensions
+    {
+        public static string InsertionSort (this string unsortedString)
+        {
+            return unsortedString;
+        }
+    }
+}
